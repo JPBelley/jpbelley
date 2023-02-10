4c077528af2b04f04ef0cfcd91e4033a52d7d0d4
jean-philippe f belley <jean-philippefbelley@jean-philippes-MacBook-Pro.local>
2023-02-10 16:46:21 -0500

    Change home and add bd shaders

--- src/components/home/hero.js
+++ src/components/home/hero.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import ThreeSphere from "../three/sphere"
 
 const Hero = () => {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,15 @@
 import * as React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
-import Hero from "../components/hero/hero"
+import Hero from "../components/home/hero"
 import Card from "../components/card/card"
 import Seo from "../components/seo"
 import MailchimpSignup from "../components/mailchimp/mailchimp"
-import * as styles from "../components/index.module.css"
+// import * as styles from "../components/index.module.css"
 import { useStaticQuery, graphql } from "gatsby"
 
-const links = [
-  {
-    text: "Learn now",
-    url: "page-2",
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-  },
-]
-
-const samplePageLinks = [
-  {
-    text: "Page 2",
-    url: "page-2",
-    badge: false,
-    description:
-      "A simple example of linking to another page within a Gatsby site",
-  },
-  { text: "TypeScript", url: "using-typescript" },
-]
 
 const IndexPage = () => {
   const fragments = useStaticQuery(graphql`
@@ -58,7 +39,7 @@ const IndexPage = () => {
             <div className="column">
               <h2>Want to learn coding?</h2>
               <p>Allow me to guide you on a journey of discovery and development, as we explore the intricacies of web creation together.</p>
-              <p>I am thrilled to announce that I am currently crafting a comprehensive course, designed to empower individuals with the knowledge and skills to code their very own website. The course is progressing smoothly, but for those eager to delve in, a beta version is currently open for enrollment. I have also created a <a href="https://discord.gg/2A9ww4gDTT" target="_blank">Discord</a> channel for any inquiries or feedback related to the course, providing a community for learning and growth. Join me in this exciting journey of web development!</p>
+              <p>I am thrilled to announce that I am currently crafting a comprehensive course, designed to empower individuals with the knowledge and skills to code their very own website. The course is progressing smoothly, but for those eager to delve in, a beta version is currently open for enrollment. I have also created a <a href="https://discord.gg/2A9ww4gDTT" target="_blank" rel="noreferrer">Discord</a> channel for any inquiries or feedback related to the course, providing a community for learning and growth. Join me in this exciting journey of web development!</p>
               {/* <Link className="button button-outline" to="page-2">Learn now</Link> */}
               <MailchimpSignup  />
             </div>
@@ -97,76 +78,18 @@ const IndexPage = () => {
               className="columns columns-3 small-gap"
               style={{marginTop: '70px'}}
             >
-              <div className="column">
-                <Card 
-                  title="Fragment Shader #1"
-                  // date="29/01/2023"
-                  fragment={`
-                    uniform float time;
-                    varying vec2 vUv;
-
-                    void main() {
-                        vec2 p = vUv;
-                        float c = cos(time);
-                        float s = sin(time);
-                        vec2 newUV = vec2(c * p.x - s * p.y, s * p.x + c * p.y);
-                        vec3 color = vec3(newUV, 0.95 + 0.1 * sin(time + length(p)));
-                        gl_FragColor = vec4(color, 1.0);
-                    }
-                  `}
-                />
-              </div>
-              <div className="column">
-                <Card 
-                  title="Fragment Shader #2"
-                  // date="03/02/2023"
-                  fragment={`
-                    precision mediump float;
-
-                    uniform float time;
-                    varying vec2 vUv;
-
-                    void main() {
-                      vec2 p = vUv;
-                      float c = cos(time * 5.0);
-                      float s = sin(time * 5.0);
-                      vec2 newUV = vec2(c * p.x - s * p.y, s * p.x + c * p.y);
-                      vec3 color = vec3(0.0);
-                      color.r = 0.95 + 0.1 * sin(time + length(newUV));
-                      color.g = 0.95 + 0.1 * cos(time + length(p));
-                      color.b = 0.95 + 0.1 * sin(time + length(newUV) + length(p));
-                      color = mix(color, vec3(1.0, 0.0, 1.0), 0.5 * sin(time * 1.0));
-                      color = mix(color, vec3(0.0, 1.0, 0.0), 0.5 * cos(time * 1.0));
-                      gl_FragColor = vec4(color, 1.0);
-                    }
-                  `}
-                />
-              </div>
-
-              <div className="column">
-                <Card 
-                  title="Fragment Shader #3"
-                  // date="20/02/2023"
-                  fragment={`
-                    precision mediump float;
-
-                    uniform float time;
-                    varying vec2 vUv;
-
-                    void main() {
-                      vec2 p = vUv;
-                      float c = cos(time);
-                      float s = sin(time);
-                      vec2 newUV = vec2(c * p.x - s * p.y, s * p.x + c * p.y);
-                      vec3 color = vec3(newUV, 0.95 + 0.1 * sin(time + length(p)));
-                      color += vec3(sin(p.x * 10.0 + time), cos(p.y * 10.0 + time), 0.0) * 0.1;
-                      color = mix(color, vec3(1.0), step(sin(time * 20.0), 0.5));
-                      color = mix(color, vec3(0.0), step(sin(time * 40.0), 0.5));
-                      gl_FragColor = vec4(color, 1.0);
-                    }`
-                  }
-                />
-              </div>
+              {fragments.allMongodbJpbelleyFragments.edges.map((fragment, index) => 
+                <div 
+                  className="column"
+                  key={`fragment-${index}`}
+                >
+                  <Card 
+                    title={fragment.node.name}
+                    // date="29/01/2023"
+                    fragment={fragment.node.code}
+                  />
+                </div>
+              )}
             </div>
           </div>
         </section>
